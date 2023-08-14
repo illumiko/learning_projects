674023f4c62e597e9abb24a6385d39f038680598
illumiko <eyanat.rayannjr@gmail.com>
2023-08-14 15:55:32 +0600

    Python:Advent of code
    "Working on day2"

---
+++ python/advent_of_code_2022/day1/a_main.py
@@ -0,0 +1,49 @@
+# Day 1: Calorie Counting (https://adventofcode.com/2022/day/1)
+# Conditions:
+# - The input file represents inventories
+# - Inventory consists of calories seperated by new line
+# - Each inventory is seperated by empty line
+
+def main():
+    print("Advent of Code - Day 1")
+    file_task_1 = open('input.csv', 'r')
+    file_task_2 = open('input.csv', 'r')
+    task_1(file_task_1)
+    task_2(file_task_2)
+
+
+# Task 1: Get the calorie number of an inventory with the most calories
+def task_1(file):
+    # Iterate over all calories, group them in inventories and after finishing grouping, compare it with
+    # the previous one
+    inventory = 0
+    biggest_inventory = 0
+    for line in file.readlines():
+        if line == "\n":  # The line is empty thus is end of one inventory
+            if inventory > biggest_inventory:
+                biggest_inventory = inventory
+            inventory = 0
+        else:  # Otherwise it represents calories
+            inventory += int(line)
+    print("Task 1 result: " + str(biggest_inventory))
+    file.close()
+
+
+# Task 2: Sum top three inventories by calorie count
+def task_2(file):
+    # First steps are the same as above, but instead of comparing, it'll be added to a list.
+    inventory = 0
+    inventory_list = []
+    for line in file.readlines():
+        if line == "\n":
+            inventory_list.append(inventory)
+            inventory = 0
+        else:
+            inventory += int(line)
+    sum_top_inventories = sum(sorted(inventory_list)[-3:])  # Sort the list and sum last 3 entries (the highest ones)
+    print("Task 2 result: " + str(sum_top_inventories))
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
--- python/advent_of_code_2022/day2/main.py
+++ python/advent_of_code_2022/day2/main.py
@@ -3,10 +3,29 @@
 # C,Z = Scissors 3 point
 # Win = 6 point, Draw = 3 point, Lose = 0 point
 
-strategy_guide = {
-    "A":"Z", 
-    "B":"X", 
-    "C":"X", 
+# strategy_guide = {
+#     "A":"Y", 
+#     "B":"X", 
+#     "C":"Z", 
+# }
+guide = [
+    "B Y",
+    "C X",
+    "A Z",
+]
+guide_from_file = []
+
+file = open("input.txt","r")
+for lines in file:
+    guide_from_file.append(lines)
+file.close()
+
+
+point_chart = { "A":1, "X":1, "B":2, "Y":2, "C":3, "Z":3, }
+
+player_points = {
+    "p1":{"Name":"John","points":0},
+    "p2":{"Name":"YOU","points":0},
 }
 
 win_condtion = {
@@ -18,32 +37,45 @@ win_condtion = {
 draw_conditon = {
     "A":"X",
     "B":"Y",
-    "C":"Z"
-}
-
-point_chart = { "A":1, "X":1, "B":2, "Y":2, "C":3, "Z":3, }
-
-player_points = {
-    "p1":{"Name":"John","points":0},
-    "p2":{"Name":"Joey","points":0},
+    "C":"Z",
 }
 
 def play(p1,p2):
     if p2 in win_condtion[p1]:
-        print(p1, "wins")
         player_points["p1"]['points'] += 6 + point_chart[p1]
         player_points["p2"]['points'] += 0 + point_chart[p2]
     elif p2 == draw_conditon[p1]:
-        print("Draw")
-        player_points["p1"]['points'] += 0 + point_chart[p1]
-        player_points["p2"]['points'] += 0 + point_chart[p2]
+        player_points["p1"]['points'] += 3 + point_chart[p1]
+        player_points["p2"]['points'] += 3 + point_chart[p2]
     else:
-        print(p2, "wins")
         player_points["p1"]['points'] += 0 + point_chart[p1]
         player_points["p2"]['points'] += 6 + point_chart[p2]
 
-for p1,p2 in strategy_guide.items():
-    play(p1,p2)
+def task1(input:list):
+
+    for i in input:
+        p1_moves = i[0] 
+        p2_moves = i[2]
+        play(p1_moves,p2_moves)
+    __import__('pprint').pprint(player_points)
+
+def task2(input:list):
+    p2 = ""
+    for x in input:
+        if x[2] == "Y":
+            p2 = draw_conditon[x[0]]
+            print("I draw")
+        if x[2] == "X":
+            p2 = win_condtion[x[0]][0]
+            print("I lost")
+        if x[2]=="Z":
+            p2 = ("A" not in win_condtion[x[0]] and "A") or ("B" not in win_condtion[x[0]] and "B") or ("C" not in win_condtion[x[0]] and "C")
+            print("I won")
+        play(x[0], p2)
 
 
+task2(guide_from_file)
 print(player_points)
+# Z is 588
+# Y is 1723
+# X is 189
