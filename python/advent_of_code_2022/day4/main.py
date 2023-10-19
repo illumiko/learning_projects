fdf9e5a038b04c37b500b4262f7aa9f1ce312d45
illumiko <eyanat.rayannjr@gmail.com>
2023-10-19 18:13:09 +0600

    Revert "Core logic implemented of day4 but there are bugs"
    This reverts commit f41d31c7285f5b51bdc9d99d36332edd9d4b3abd.

--- python/advent_of_code_2022/day4/main.py
+++ python/advent_of_code_2022/day4/main.py
@@ -1,4 +1,3 @@
-import re
 input = []
 file = open("input.txt","r")
 for lines in file:
@@ -7,29 +6,9 @@ for lines in file:
 file.close()
 
 def task1(input:list):
-    pair = 0
     for i in input:
-        isContained = False
-        pattern = r'(\d+)-(\d+)'
-        match1 = re.search(pattern, i[0])
-        match2 = re.search(pattern, i[1])
+        seperatorIndex1 = round((len(i[0])-1)/2)
+        seperatorIndex2 = round((len(i[1])-1)/2)
 
-        if match1 and match2:
-            range1 = - int(match1.group(1)) + int(match1.group(2))
-            range2 = - int(match2.group(1)) + int(match2.group(2))
-
-            # print(match1.group(1),"-",match1.group(2), range1, "|" ,match2.group(1),"-",match2.group(2),":", range2)
-
-            if range2 > range1:
-                isContained = int(match2.group(1)) < int(match1.group(1)) and int(match2.group(2)) >= int(match1.group(1))
-            elif range2 < range1:
-                isContained = int(match2.group(1)) > int(match1.group(1)) and int(match2.group(2)) < int(match1.group(2))
-            elif range2 == range1:
-                isContained = int(match2.group(1)) == int(match1.group(1)) and int(match2.group(2)) == int(match1.group(2))
-
-            if isContained:
-                pair+=1
-
-    print(pair)
 
 task1(input)
