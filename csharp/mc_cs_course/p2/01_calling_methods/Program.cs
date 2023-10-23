73b2edfd1a1128234edd83bbce7cf02014f69392
illumiko <eyanat.rayannjr@gmail.com>
2023-10-24 02:04:04 +0600

    C#:New filenames, completed 01

---
+++ csharp/mc_cs_course/p2/01_calling_methods/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Just some code based on the aritcals preceeding challenge */
+Random dice = new Random();
+int roll = dice.Next();
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+/* Console.WriteLine($"{roll},{roll1},{roll2}"); */
+
+/* Code challenge: Implement a method of the Math class that returns the larger of two numbers */
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
--- csharp/mc_cs_course/p2/calling_methods/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Random dice = new Random();
-int roll = dice.Next(1,7);
-int roll1 = dice.Next(1,7);
-int roll2 = dice.Next(1,7);
-Console.WriteLine($"{roll},{roll1},{roll2}");
